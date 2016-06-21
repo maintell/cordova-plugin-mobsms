73cac20496644c7d6eb67d79c9347f1009f4049b
maintell <maintell@gmail.com>
2016-06-22 00:34:44 +0800

    sss

--- src/android/mobsms.java
+++ src/android/mobsms.java
@@ -1,12 +1,20 @@
 package com.jlg.mobsms;
 
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import android.content.Context;
+import android.telephony.TelephonyManager;
+import android.text.TextUtils;
+import android.util.Log;
 
+import org.json.JSONObject;
 import cn.smssdk.EventHandler;
+import cn.smssdk.OnSendMessageHandler;
 import cn.smssdk.SMSSDK;
 
 /**
@@ -23,7 +31,50 @@ public class mobsms extends CordovaPlugin {
 	
 	private static String PhoneNumber = "";
 	private static String VerifyCode = "";
-	
+
+
+    private CallbackContext requestcodeCallbackContext = null;
+    private CallbackContext submitcodeCallbackContext = null;
+
+    private OnSendMessageHandler osmHandler = new OnSendMessageHandler() {
+        @Override
+        public boolean onSendMessage(String s, String s1) {
+            Log.d("OnSendMessageHandler",s+"|"+s1);
+            return true;
+        }
+    };
+
+
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        SMSSDK.initSDK(cordova.getActivity().getApplicationContext(), APPKEY , APPSECRET);
+        EventHandler eh=new EventHandler(){
+            @Override
+            public void afterEvent(int event, int result, Object data) {
+                if (result == SMSSDK.RESULT_COMPLETE) {
+                    //回调完成
+                    if (event == SMSSDK.EVENT_SUBMIT_VERIFICATION_CODE) {
+                        //提交验证码成功
+                        PluginResult submitcodeResult = new PluginResult(PluginResult.Status.OK, data.toString());
+                        submitcodeResult.setKeepCallback(true);
+                        submitcodeCallbackContext.sendPluginResult(submitcodeResult);
+                    }else if (event == SMSSDK.EVENT_GET_VERIFICATION_CODE){
+                        //获取验证码成功
+                        PluginResult requestcodeResult = new PluginResult(PluginResult.Status.OK, data.toString());
+                        requestcodeResult.setKeepCallback(true);
+                        requestcodeCallbackContext.sendPluginResult(requestcodeResult);
+
+                    }else if (event ==SMSSDK.EVENT_GET_SUPPORTED_COUNTRIES){
+                        //返回支持发送验证码的国家列表
+                    }
+                }else{
+                    ((Throwable)data).printStackTrace();
+
+                }
+            }
+        };
+        SMSSDK.registerEventHandler(eh); //注册短信回调
+    }
     /**
      * Constructor.
      */
@@ -40,48 +91,64 @@ public class mobsms extends CordovaPlugin {
      * @return                  True when the action was valid, false otherwise.
      */
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("DoVerify")) {
-            this.DoVerify(args.getJSONObject(0).toString());
-        }       
+        if (action.equals("RequestVerifyCode")) {
+            requestcodeCallbackContext = callbackContext;
+            this.RequestVerifyCode(args.getJSONObject(0).toString());
+        }
+        else if(action.equals("SubmitVerifyCode"))
+        {
+            submitcodeCallbackContext = callbackContext;
+            this.SubmitVerifyCode(args.getJSONObject(0).toString());
+        }
         else if (action.equals("GetVerifyCode")) {
             JSONObject r = new JSONObject();
             r.put("Phone", mobsms.PhoneNumber);
             r.put("VerifyCode", mobsms.VerifyCode);           
-            callbackContext.success(r);        }
+            callbackContext.success(r);
+        }
         else {
             return false;
         }
         return true;
     }
 
-    
-    public void DoVerify(String Phone) {
-       
+    public void RequestVerifyCode(String Phone) {
+        SMSSDK.getVerificationCode("86", Phone.trim(), osmHandler);
     }
 
-	
-	private void initSDK() {
-		SMSSDK.initSDK(this,APPKEY,APPSECRET);
-        EventHandler eh=new EventHandler(){ 
-            @Override
-            public void afterEvent(int event, int result, Object data) {
- 
-               if (result == SMSSDK.RESULT_COMPLETE) {
-                //回调完成
-                if (event == SMSSDK.EVENT_SUBMIT_VERIFICATION_CODE) {
-                //提交验证码成功
-                }else if (event == SMSSDK.EVENT_GET_VERIFICATION_CODE){
-                //获取验证码成功
-                }else if (event ==SMSSDK.EVENT_GET_SUPPORTED_COUNTRIES){
-                //返回支持发送验证码的国家列表
-                } 
-                }else{                                                                 
-                 ((Throwable)data).printStackTrace(); 
-                }
-            } 
-        }; 
-        SMSSDK.registerEventHandler(eh); //注册短信回调
-	}
+    public void SubmitVerifyCode(String code) {
+        SMSSDK.submitVerificationCode("86",mobsms.PhoneNumber, code);
+    }
+
+
+
+
+//    private String[] getCurrentCountry() {
+//        String mcc = getMCC();
+//        String[] country = null;
+//        if (!TextUtils.isEmpty(mcc)) {
+//            country = SMSSDK.getCountryByMCC(mcc);
+//        }
+//
+//        if (country == null) {
+//            Log.w("SMSSDK", "no country found by MCC: " + mcc);
+//            country = SMSSDK.getCountry(DEFAULT_COUNTRY_ID);
+//        }
+//        return country;
+//    }
+//
+//
+//    private String getMCC() {
+//        TelephonyManager tm = (TelephonyManager) activity.getSystemService(Context.TELEPHONY_SERVICE);
+//        // 返回当前手机注册的网络运营商所在国家的MCC+MNC. 如果没注册到网络就为空.
+//        String networkOperator = tm.getNetworkOperator();
+//        if (!TextUtils.isEmpty(networkOperator)) {
+//            return networkOperator;
+//        }
+//
+//        // 返回SIM卡运营商所在国家的MCC+MNC. 5位或6位. 如果没有SIM卡返回空
+//        return tm.getSimOperator();
+//    }
     
    
 }
--- www/mobsms.js
+++ www/mobsms.js
@@ -1,121 +1,72 @@
-/*
- *
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- *
-*/
+var cordova = require('cordova'),
+    exec = require('cordova/exec');
 
-var exec = require('cordova/exec');
+var STATUS_CRITICAL = 5;
+var STATUS_LOW = 20;
 
-/**
- * Provides access to the vibration mechanism on the device.
- */
-
-module.exports = {
-
-    /**
-     * Vibrates the device for a given amount of time or for a given pattern or immediately cancels any ongoing vibrations (depending on the parameter).
-     *
-     * @param {Integer} param       The number of milliseconds to vibrate (if 0, cancels vibration)
-     *
-     *
-     * @param {Array of Integer} param    Pattern with which to vibrate the device.
-     *                                      Pass in an array of integers that
-     *                                      are the durations for which to
-     *                                      turn on or off the vibrator in
-     *                                      milliseconds. The FIRST value
-     *                                      indicates the
-     *                                      number of milliseconds for which
-     *                                      to keep the vibrator ON before
-     *                                      turning it off. The NEXT value indicates the
-     *                                      number of milliseconds for which
-     *                                      to keep the vibrator OFF before
-     *                                      turning it on. Subsequent values
-     *                                      alternate between durations in
-     *                                      milliseconds to turn the vibrator
-     *                                      off or to turn the vibrator on.
-     *                                      (if empty, cancels vibration)
-     */
-    vibrate: function(param) {
-
-        /* Aligning with w3c spec */
-        
-        //vibrate
-        if ((typeof param == 'number') && param !== 0)
-            exec(null, null, "Vibration", "vibrate", [param]);
-
-        //vibrate with array ( i.e. vibrate([3000]) )
-        else if ((typeof param == 'object') && param.length == 1)
-        {
-            //cancel if vibrate([0])
-            if (param[0] === 0)
-                exec(null, null, "Vibration", "cancelVibration", []);
-
-            //else vibrate
-            else
-                exec(null, null, "Vibration", "vibrate", [param[0]]);
-        }
-
-        //vibrate with a pattern
-        else if ((typeof param == 'object') && param.length > 1)
-        {
-            var repeat = -1; //no repeat
-            exec(null, null, "Vibration", "vibrateWithPattern", [param, repeat]);
-        }
-
-        //cancel vibration (param = 0 or [])
-        else
-            exec(null, null, "Vibration", "cancelVibration", []);
-
-        return true;
-    },
-
-    /**
-     * Vibrates the device with a given pattern.
-     *
-     * @param {Array of Integer} pattern    Pattern with which to vibrate the device.
-     *                                      Pass in an array of integers that
-     *                                      are the durations for which to
-     *                                      turn on or off the vibrator in
-     *                                      milliseconds. The first value
-     *                                      indicates the number of milliseconds
-     *                                      to wait before turning the vibrator
-     *                                      on. The next value indicates the
-     *                                      number of milliseconds for which
-     *                                      to keep the vibrator on before
-     *                                      turning it off. Subsequent values
-     *                                      alternate between durations in
-     *                                      milliseconds to turn the vibrator
-     *                                      off or to turn the vibrator on.
-     *
-     * @param {Integer} repeat              Optional index into the pattern array at which
-     *                                      to start repeating (will repeat until canceled),
-     *                                      or -1 for no repetition (default).
-     */
-    vibrateWithPattern: function(pattern, repeat) {
-        repeat = (typeof repeat !== "undefined") ? repeat : -1;
-        pattern.unshift(0); //add a 0 at beginning for backwards compatibility from w3c spec
-        exec(null, null, "Vibration", "vibrateWithPattern", [pattern, repeat]);
-    },
-
-    /**
-     * Immediately cancels any currently running vibration.
-     */
-    cancelVibration: function() {
-        exec(null, null, "Vibration", "cancelVibration", []);
+var Mobsms = function() {
+    this._level = null;
+    this._isPlugged = null;
+    // Create new event handlers on the window (returns a channel instance)
+    this.channels = {
+      mobsms_requeststatus:cordova.addWindowEventHandler("mobsms_requeststatus"),
+      mobsms_codestatus:cordova.addWindowEventHandler("mobsms_codestatus")
+    };
+    for (var key in this.channels) {
+        this.channels[key].onHasSubscribersChange = Mobsms.onHasSubscribersChange;
     }
 };
+
+function handlers() {
+    return mobsms.channels.mobsms_requeststatus.numHandlers +
+        mobsms.channels.mobsms_codestatus.numHandlers ;
+}
+
+/**
+ * Event handlers for when callbacks get registered for the Mobsms.
+ * Keep track of how many handlers we have so we can start and stop the native Mobsms listener
+ * appropriately (and hopefully save on Mobsms life!).
+ */
+//Mobsms.onHasSubscribersChange = function() {
+//  // If we just registered the first handler, make sure native listener is started.
+//  if (this.numHandlers === 1 && handlers() === 1) {
+//      exec(mobsms._status, mobsms._error, "Mobsms", "start", []);
+//  } else if (handlers() === 0) {
+//      exec(null, null, "Mobsms", "stop", []);
+//  }
+//};
+
+Mobsms.RequestVerifyCode = function(phone)
+  {
+       exec(mobsms._requestcodeCallback, mobsms._error, "mobsms", "RequestVerifyCode", [phone]);
+  }
+
+  Mobsms.prototype._requestcodeCallback = function (info) {
+
+      if (info) {
+          cordova.fireWindowEvent("mobsms_requeststatus", info);
+      }
+  };
+
+Mobsms.SubmitVerifyCode = function(code)
+{
+     exec(mobsms._submitcodeCallback, mobsms._error, "mobsms", "SubmitVerifyCode", [code]);
+}
+
+Mobsms.prototype._submitcodeCallback = function (info) {
+    if (info) {
+        cordova.fireWindowEvent("mobsms_codestatus", info);
+    }
+};
+
+
+/**
+ * Error callback for Mobsms start
+ */
+Mobsms.prototype._error = function(e) {
+    console.log("Error initializing Mobsms: " + e);
+};
+
+var mobsms = new Mobsms(); // jshint ignore:line
+
+module.exports = mobsms;
